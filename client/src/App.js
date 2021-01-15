f48d3ca14ec2595229664273ef5e4d880d1d05ff
omfuke <46218908+omfuke@users.noreply.github.com>
2021-01-16 01:36:28 +0530

    client and server side running

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <div>Hello world</div>
+    </Router>
+  );
+};
+
+export default App;
