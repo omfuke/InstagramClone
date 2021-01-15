176b7b31b2111deef9ba6da38f42072add9a8af2
omfuke <46218908+omfuke@users.noreply.github.com>
2021-01-16 05:07:19 +0530

    tailwind added

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
     <Router>
-      <div>Hello world</div>
+      <div className="bg-red-500">Hello world</div>
     </Router>
   );
 };
