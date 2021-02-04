cb187409d5d68b1cfe0423c40062bf602126191f
omfuke <46218908+omfuke@users.noreply.github.com>
2021-02-05 03:35:41 +0530

    profile page

--- client/src/layout/Profile.js
+++ client/src/layout/Profile.js
@@ -4,10 +4,19 @@ import { connect } from "react-redux";
 import { getCurrentProfile } from "../actions/profile";
 import "./profile.css";
 import Upload from "./Upload";
+import { Redirect } from "react-router";
+import axios from "axios";
 
-const Profile = ({ profile, getCurrentProfile, post }) => {
+const Profile = ({
+  profile,
+  user,
+  getCurrentProfile,
+  isAuthencticated,
+  post,
+}) => {
   const [image, setImage] = useState(false);
   const [dp, setDp] = useState(null);
+  const [p, setP] = useState([]);
 
   useEffect(() => {
     getCurrentProfile();
@@ -20,16 +29,40 @@ const Profile = ({ profile, getCurrentProfile, post }) => {
   const dpHandler = (url) => {
     setDp(url);
   };
+  if (!isAuthencticated) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <>
       <Navbar />
-
-      <div className="profile">
-        <div className="profileImg">
-          <div></div>
+      <div className="profileHeader">
+        <div className="profile">
+          <div className="profileImg">
+            <div></div>
+          </div>
+          <div className="profileDetail">
+            <div className="profileDetail1">
+              <span className="profileUser" style={{ marginLeft: "1em" }}>
+                {user.name}
+              </span>
+              <span className="btn btn-primary" style={{ marginLeft: "1em" }}>
+                Create Profile
+              </span>
+            </div>
+            <div>
+              <span style={{ marginLeft: "1em" }}>0 : posts</span>
+              <span style={{ marginLeft: "2em" }}>0 : followers</span>
+              <span style={{ marginLeft: "2em" }}>0 : following</span>
+            </div>
+            <div>
+              <p style={{ marginLeft: "1em" }}>VJTIAN messi of VJTI</p>
+            </div>
+          </div>
         </div>
-        <div className="profileDetail"></div>
+      </div>
+      <div className="profilePosts">
+        <div></div>
       </div>
       {profile && (
         <div>
@@ -69,7 +102,12 @@ const Profile = ({ profile, getCurrentProfile, post }) => {
 };
 
 const mapStateToProps = (state) => {
-  return { profile: state.profile.profile, post: state.profile.posts };
+  return {
+    user: state.auth.user,
+    isAuthencticated: state.auth.isAuthencticated,
+    profile: state.profile.profile,
+    post: state.profile.posts,
+  };
 };
 
 export default connect(mapStateToProps, { getCurrentProfile })(Profile);
