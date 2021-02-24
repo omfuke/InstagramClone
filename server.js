ae6510f05670f1de15d6b4594193f1be1984f9a6
omfuke <46218908+omfuke@users.noreply.github.com>
2021-02-24 18:52:32 +0530

    'deployment'

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 connectDB();
 
 app.use(cors());
 
 app.use(express.json({ extended: false }));
 app.use(express.static("uploads"));
-app.get("/", (req, res) => res.send("API RUNNING"));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
