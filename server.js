90631e0b8d6546a354d1f9cd8cf6040c15978774
omfuke <46218908+omfuke@users.noreply.github.com>
2021-02-25 00:11:00 +0530

    deploy99

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const profile = require("./routes/api/profile");
 connectDB();
 
 app.use(cors());
-app.use(express.static("public"));
 
 app.use(express.json({ extended: false }));
-app.use(express.static("uploads"));
+
 app.get("/", (req, res) => {
   return res.json({ msg: "mkc" });
 });
@@ -23,14 +22,14 @@ app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/post", post);
 
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
