af26102ea866b1e6af9bec4e46ff4dbb834e6fec
omfuke <46218908+omfuke@users.noreply.github.com>
2021-02-25 00:29:46 +0530

    DEV

--- server.js
+++ server.js
@@ -14,22 +14,19 @@ app.use(cors());
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  return res.json({ msg: "mkc" });
-});
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/post", post);
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
